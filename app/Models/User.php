1702389f3ba4c6d1a8c4d492051570d89e532773
mhd-amirul <amirulm236@gmail.com>
2023-01-12 09:25:57 +0800

    feat: new EmployeeFired function

--- app/Models/User.php
+++ app/Models/User.php
@@ -41,4 +41,12 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function employeeFired()
+    {
+        $this->employee_id = null;
+        $this->save();
+
+        return $this;
+    }
 }
