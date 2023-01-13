f5d526b9b9e5ef0c67b6283e5e366291601277ca
mhd-amirul <amirulm236@gmail.com>
2023-01-13 17:13:34 +0800

    feat: like, unlike, toogle, and count like on post

--- tests/Feature/V2/AppStoreTest.php
+++ tests/Feature/V2/AppStoreTest.php
@@ -2,7 +2,8 @@
 
 namespace Tests\Feature\V2;
 
-use App\Models\Rate;
+use App\Models\Post;
+use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
@@ -12,15 +13,71 @@ class AppStoreTest extends TestCase
     use RefreshDatabase;
 
     /** @test */
-    public function check_the_most_popular_store()
+    public function user_can_like_post()
     {
-        Rate::factory(3)->create();
-        Rate::factory()->create(["like" => 20]);
-        $like = Rate::factory()->create(["like" => 50]);
+        $post = Post::factory()->create();
+        $user = User::factory()->create();
 
-        $popular = Rate::popular();
+        $this->actingAs($user);
 
-        $this->assertEquals($like->id, $popular->first()->id);
-        $this->assertCount(3, $popular);
+        $post->like();
+
+        $this->assertDatabaseHas('likes', [
+            "user_id" => $user->id,
+            "likeable_id" => $post->id,
+            "likeable_type" => get_class($post)
+        ]);
+
+        $this->assertTrue($post->isLiked());
+    }
+
+    /** @test */
+    public function user_can_unlike_post()
+    {
+        $post = Post::factory()->create();
+        $user = User::factory()->create();
+
+        $this->actingAs($user);
+
+        $post->like();
+        $post->unlike();
+
+        $this->assertDatabaseMissing('likes', [
+            "user_id" => $user->id,
+            "likeable_id" => $post->id,
+            "likeable_type" => get_class($post)
+        ]);
+
+        $this->assertFalse($post->isLiked());
+    }
+
+    /** @test */
+    public function user_toggle_like_or_unlike_on_post()
+    {
+        $post = Post::factory()->create();
+        $user = User::factory()->create();
+
+        $this->actingAs($user);
+
+        $post->toggle();
+
+        $this->assertTrue($post->isLiked());
+
+        $post->toggle();
+
+        $this->assertFalse($post->isLiked());
+    }
+
+    /** @test */
+    public function count_how_many_like_on_post()
+    {
+        $post = Post::factory()->create();
+        $user = User::factory()->create();
+
+        $this->actingAs($user);
+
+        $post->toggle();
+
+        $this->assertEquals(1, $post->getLikesCountAttribute());
     }
 }
