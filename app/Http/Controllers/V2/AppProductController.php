8323019292e7f301f9a9b6e48464a0de24e8e4f2
mhd-amirul <amirulm236@gmail.com>
2023-02-02 09:08:03 +0800

    feat: customer validation.

--- app/Http/Controllers/V2/AppProductController.php
+++ app/Http/Controllers/V2/AppProductController.php
@@ -4,13 +4,14 @@ namespace App\Http\Controllers\V2;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class AppProductController extends Controller
 {
     protected $name;
     protected $cost;
 
-    public function __construct($name, $cost)
+    public function __construct($name = null, $cost = null)
     {
         $this->name = $name;
         $this->cost = $cost;
@@ -22,5 +23,17 @@ class AppProductController extends Controller
             "name" => $this->name,
             "cost" => $this->cost,
         ];
+    }
+
+    public function costumerService()
+    {
+        request()->validate([
+            "name" => "required",
+            "email" => "required|email",
+            "question" => "required",
+            "verification" => "required|in:5,five",
+        ]);
+
+        return response()->json(["status" => "success", "item" => request()], 200);
     }
 }
